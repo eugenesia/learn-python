53aee00b9e5c34b36ccb167dbc8612144f1c0b63
Eugene Sia <eugene@eugenesia.co.uk>
2025-07-18 16:56:13 +0100

    Interpolate strings using f"..."

--- hello_world.py
+++ hello_world.py
@@ -8,3 +8,8 @@ print(message)
 name = "Ada Lovelace"
 print(name.upper())
 print(name.lower())
+
+first_name = "ada"
+last_name = "lovelace"
+full_name = f"Full name interpolation: {first_name} {last_name}"
+print(full_name)
